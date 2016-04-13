911f0ebc756b31d960ae789051e1dfa84cd2d247
Ash Matadeen <ash.matadeen@gmail.com>
2016-04-13 11:29:28 +0100

    register the cpt

--- waggle-redirects.php
+++ waggle-redirects.php
@@ -8,10 +8,41 @@
  * Version: 1.0
  */
 
-add_filter( 'title_save_pre', 'waggle_save_title' );
+
 function waggle_save_title( $title ) {
 	if ( $_POST['post_type'] == 'redirect' ){
 		$title = $_POST['fields']['field_570e1a44d5123'];
 	}
 	return $title;
 }
+add_filter( 'title_save_pre', 'waggle_save_title' );
+
+function waggle_register_redirect_cpt() {
+	$labels = array(
+		'name' => 'Redirects',
+		'singular_name' => 'Redirect',
+		);
+
+	$args = array(
+		'label' => 'Redirects',
+		'labels' => $labels,
+		'description' => '',
+		'public' => false,
+		'show_ui' => true,
+		'show_in_rest' => false,
+		'rest_base' => '',
+		'has_archive' => false,
+		'show_in_menu' => true,
+		'exclude_from_search' => true,
+		'capability_type' => 'post',
+		'map_meta_cap' => true,
+		'hierarchical' => false,
+		'rewrite' => false,
+		'query_var' => false,
+		'menu_icon' => 'dashicons-arrow-right-alt',		
+		'supports' => false,				
+	);
+	register_post_type( 'redirect', $args );
+}
+add_action( 'init', 'waggle_register_redirect_cpt' );
+
